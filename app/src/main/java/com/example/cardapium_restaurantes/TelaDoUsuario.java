84011ec69f14095171042a1ce5ac4bf45d1e0cda
Breno Vergilio <bjvergilio47@gmail.com>
2021-06-17 18:00:16 -0400

    feat: logout implementado na tela do usuário Implemented-by: Breno Vergílio refs #2

--- app/src/main/java/com/example/cardapium_restaurantes/TelaDoUsuario.java
+++ app/src/main/java/com/example/cardapium_restaurantes/TelaDoUsuario.java
@@ -2,13 +2,31 @@ package com.example.cardapium_restaurantes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class TelaDoUsuario extends AppCompatActivity {
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_do_usuario);
+
+        logout = findViewById(R.id.bttLogout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                Toast.makeText(getApplicationContext(), "Até mais!", Toast.LENGTH_LONG);
+            }
+        });
     }
 }
